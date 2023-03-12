afb0bbd2313be35e06652ace8789048744032053
Antonio-74 <antoniolopezvalles604@gmail.com>
2023-03-12 16:24:11 -0700

    navbar created

--- vawulens/src/components/Navbar.jsx
+++ vawulens/src/components/Navbar.jsx
@@ -1,34 +1,47 @@
 import React from 'react';
+import { useState } from 'react';
 import './navbar.css';
 import Logo from '../images/logo.png';
 import { Link, NavLink } from 'react-router-dom';
 import { links } from '../data.js';
 import { GoThreeBars } from 'react-icons/go';
+import { MdOutlineClose } from 'react-icons/md';
 
 const Navbar = () => {
-  return (
-    <nav>
-        <div className="container nav__container">
-            <Link to='/' className='logo'>
-                <img src={Logo} alt='Nav Logo' />
-            </Link>
-            <ul className='nav__links'>
-                {
-                    links.map(({name, path}, index) => {
-                        return (
-                            <li>
-                                <NavLink to={path}>{name}</NavLink>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
-            <button className='nav__toggle-btn'>
-                <GoThreeBars />
-            </button>
-        </div>
-    </nav>
-  )
+    const [isNavShowing, setIsNavShowing] = useState(false);
+
+    return (
+        <nav>
+            <div className="container nav__container">
+                <Link to='/' className='logo' onClick={() => setIsNavShowing(false)}>
+                    <img src={Logo} alt='Nav Logo' />
+                </Link>
+                <ul className={`nav__links ${isNavShowing ? 'show__nav' : 'hide__nav'}`}>
+                    {
+                        links.map(({name, path}, index) => {
+                            return (
+                                <li key={index}>
+                                    <NavLink 
+                                        to={path} 
+                                        className={({isActive}) => isActive ? 'active-nav' : ''}
+                                        onClick={() => setIsNavShowing(prev => !prev)}
+                                    >
+                                        {name}
+                                    </NavLink>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+                <button 
+                    className='nav__toggle-btn' 
+                    onClick={() => setIsNavShowing(prev => !prev)}
+                >
+                    {isNavShowing ? <MdOutlineClose /> : <GoThreeBars />}
+                </button>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
--- vawulens/src/pages/home/Home.jsx
+++ vawulens/src/pages/home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from '../../components/Header';
 import './home.css';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <Header />
+    </>
   )
 }
 
